07cfa5f5dc86ba73074c26d0358bbfbbc92f5ab2
oneananda <anandathirumurugan@gmail.com>
2024-10-29 22:49:46 +0530

    Implement SentimentIntensityAnalyzer

--- Text-Processing/Text-Sentiment-Analyzer/text_sentiment_analyzer.py
+++ Text-Processing/Text-Sentiment-Analyzer/text_sentiment_analyzer.py
@@ -2,24 +2,21 @@
 This program contains functions for performing Natural Language Processing (NLP).
 """
 
+from nltk.sentiment import SentimentIntensityAnalyzer
+
 import nltk
 nltk.download('vader_lexicon')
 
-from nltk.sentiment import SentimentIntensityAnalyzer
-
 def analyze_sentiment(TEXT):
     SIA = SentimentIntensityAnalyzer()
-    SENTIMENT_SCORES = SIA.polarity_scores(TEXT)
-    
+    SENTIMENT_SCORES = SIA.polarity_scores(TEXT)    
     if SENTIMENT_SCORES['compound'] >= 0.05:
         SENTIMENT = "Positive"
     elif SENTIMENT_SCORES['compound'] <= -0.05:
         SENTIMENT = "Negative"
     else:
-        SENTIMENT = "Neutral"
-    
+        SENTIMENT = "Neutral"    
     print(f"Text: {TEXT}")
     print(f"Sentiment Scores: {SENTIMENT_SCORES}")
-    print(f"Overall Sentiment: {SENTIMENT}")
-    
+    print(f"Overall Sentiment: {SENTIMENT}")    
     return SENTIMENT_SCORES, SENTIMENT

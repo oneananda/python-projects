e03d25f4b0ace97c67c4168c3f7764fb7a3bf626
oneananda <anandathirumurugan@gmail.com>
2024-10-31 20:55:03 +0530

    C0303: Trailing whitespace (trailing-whitespace)

--- Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
+++ Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
@@ -20,10 +20,10 @@ def redact_pii(given_path):
     and redacts Personally Identifiable Information (PII).
     """
     path, file_name = os.path.split(given_path)
-    # print("Path:", PATH)       
+    # print("Path:", PATH)
     # print("Filename:", FILE_NAME)
-    content = ""	
-    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'	
+    content = ""
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
     with open(given_path,"r", encoding="utf-8") as file:
         content = file.read()
     content = re.sub(email_pattern, '[REDACTED EMAIL]', content)

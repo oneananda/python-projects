4b28efe44d9c3a5c466d5d5483d3bdedbc328d20
oneananda <anandathirumurugan@gmail.com>
2024-10-31 21:29:17 +0530

    Using os.path.join

--- Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
+++ Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
@@ -23,12 +23,35 @@ def redact_pii(given_path):
     # print("Path:", PATH)
     # print("Filename:", FILE_NAME)
     content = ""
-    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-    with open(given_path,"r", encoding="utf-8") as file:
+    
+    try:
+      with open(given_path, "r", encoding="utf-8") as file:
         content = file.read()
-    content = re.sub(email_pattern, '[REDACTED EMAIL]', content)
+    # Process the content as needed
+    except FileNotFoundError:
+        print(f"The file at {given_path} was not found.")
+    except PermissionError:
+        print(f"Permission denied for accessing the file at {given_path}.")
+    except UnicodeDecodeError:
+        print(f"Could not decode the file at {given_path}. Check the file encoding.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    content = process_content(content)
+
     random_file_name = file_name.replace(".txt",'') +'_'+ str(uuid.uuid4())[0:7]+'.txt'
-    with open(path +'\\' + random_file_name, "w", encoding="utf-8") as file:
+    new_file_path = os.path.join(path, random_file_name)
+    with open(new_file_path, "w", encoding="utf-8") as file:
         file.write(content)
     return content
+
+def process_content(content):
+    """
+    This function processes the content 
+    and redacts Personally Identifiable Information (PII).
+    """
+    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+    content = re.sub(email_pattern, '[REDACTED EMAIL]', content) 
+    return content
+
 redact_pii(ARGS.INP_PATH)

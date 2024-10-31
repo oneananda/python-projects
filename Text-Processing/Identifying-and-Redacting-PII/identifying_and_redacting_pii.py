8ac782f0642423039049ba9b06d4de8eb5337a4e
oneananda <anandathirumurugan@gmail.com>
2024-10-31 21:58:41 +0530

    W0718: Catching too general exception Exception (broad-exception-caught)

--- Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
+++ Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
@@ -23,10 +23,10 @@ def redact_pii(given_path):
     # print("Path:", PATH)
     # print("Filename:", FILE_NAME)
     content = ""
-    
+
     try:
-      with open(given_path, "r", encoding="utf-8") as file:
-        content = file.read()
+        with open(given_path, "r", encoding="utf-8") as file:
+          content = file.read()
     # Process the content as needed
     except FileNotFoundError:
         print(f"The file at {given_path} was not found.")
@@ -34,8 +34,6 @@ def redact_pii(given_path):
         print(f"Permission denied for accessing the file at {given_path}.")
     except UnicodeDecodeError:
         print(f"Could not decode the file at {given_path}. Check the file encoding.")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
 
     content = process_content(content)
 
@@ -51,8 +49,24 @@ def process_content(content):
     This function processes the content 
     and redacts Personally Identifiable Information (PII).
     """
-    email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-    content = re.sub(email_pattern, '[REDACTED EMAIL]', content) 
+    patterns = {
+        'EMAIL': r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}',
+        'PHONE': r'\b(\+?\d{1,2})?\s?(\(?\d{3}\)?)?\s?-?\d{3}-?\d{4}\b',
+        'SSN': r'\b\d{3}-\d{2}-\d{4}\b',
+        'CREDIT_CARD': r'\b(?:\d{4}-?){3}\d{4}\b',
+        'DATE_OF_BIRTH': r'\b\d{2}[/-]\d{2}[/-]\d{4}\b',
+        'PASSPORT': r'\b[0-9]{9}\b',  # Example for U.S. passport numbers
+        'DRIVER_LICENSE': r'\b[A-Z]{1,2}-\d{3,6}-\d{3,6}\b',
+        'BANK_ACCOUNT': r'\b\d{9,18}\b',
+        'IP_ADDRESS': r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b',
+        'MAC_ADDRESS': r'\b([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})\b',
+        'PHYSICAL_ADDRESS': r'\b\d+\s[A-Za-z]+(?:\s[A-Za-z]+)*\b'  # Basic pattern
+    }
+
+    # Replace identified PII with a placeholder indicating the type of PII
+    for pii_type, pattern in patterns.items():
+        content = re.sub(pattern, f'[REDACTED {pii_type}]', content)
+
     return content
 
 redact_pii(ARGS.INP_PATH)

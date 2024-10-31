10fbcee6a9bee4a29f2e6aa922a62adcb7fc6296
oneananda <anandathirumurugan@gmail.com>
2024-10-31 10:44:31 +0530

    Parsing arguments

--- Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
+++ Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
@@ -3,6 +3,18 @@ This program contains functions which identifies and redacts Personally Identifi
 """
 
 import re
+import sys, argparse
 
-def redact_pii(text):
-  return text
+#INP_PATH = sys.argv[1]
+PARSER = argparse.ArgumentParser(description="PII Redacting parameters!")
+PARSER.add_argument("INP_PATH",type=str,help="Path of the text file")
+
+ARGS = PARSER.parse_args() 
+
+def redact_pii(given_path):
+  with open(given_path,'r') as file:
+    content = file.read()
+    print(content)
+  return content
+
+redact_pii(ARGS.INP_PATH)

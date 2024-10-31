e0cad1e8d7ceb1791eb86b100dd14436b42a2195
oneananda <anandathirumurugan@gmail.com>
2024-10-31 11:20:54 +0530

    W0311: Bad indentation. Found 2 spaces, expected 4 (bad-indentation)

--- Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
+++ Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
@@ -12,19 +12,22 @@ PARSER.add_argument("INP_PATH",type=str,help="Path of the text file")
 
 ARGS = PARSER.parse_args()
 
-def redact_pii(given_path):
-  PATH, FILE_NAME = os.path.split(given_path)
-  #print("Path:", PATH)       
-  #print("Filename:", FILE_NAME)
-  EMAIL_PATTERN = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
-  with open(given_path,'r') as file:
-    CONTENT = file.read()
-  CONTENT = re.sub(EMAIL_PATTERN, '[REDACTED EMAIL]', CONTENT)
-  RANDOM_FILE_NAME = FILE_NAME.replace(".txt",'') +'_'+ str(uuid.uuid4())[0:7]+'.txt'
+def redact_pii(given_path):    
+	PATH, FILE_NAME = os.path.split(given_path)
 
-  with open(PATH +'\\' + RANDOM_FILE_NAME, 'w') as file :
-    file.write(CONTENT)
-  #print(RANDOM_FILE_NAME)
+	#print("Path:", PATH)       
+	#print("Filename:", FILE_NAME)
+	
+	EMAIL_PATTERN = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+	
+	with open(given_path,'r') as file:
+	CONTENT = file.read()
+	CONTENT = re.sub(EMAIL_PATTERN, '[REDACTED EMAIL]', CONTENT)
+	RANDOM_FILE_NAME = FILE_NAME.replace(".txt",'') +'_'+ str(uuid.uuid4())[0:7]+'.txt'
+
+	with open(PATH +'\\' + RANDOM_FILE_NAME, 'w') as file :
+	file.write(CONTENT)
+	#print(RANDOM_FILE_NAME)
   return CONTENT
 
 redact_pii(ARGS.INP_PATH)

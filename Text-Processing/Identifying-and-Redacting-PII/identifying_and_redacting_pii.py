19049adc895eee7254ab831be7d4cc761efec906
oneananda <anandathirumurugan@gmail.com>
2024-10-31 11:31:04 +0530

    'unindent does not match any outer indentation level (identifying_and_redacting_pii, line 31)' (syntax-error)

--- Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
+++ Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
@@ -8,27 +8,27 @@ import sys, argparse, os, uuid
 
 # INP_PATH = sys.argv[1]
 PARSER = argparse.ArgumentParser(description="PII Redacting parameters!")
-PARSER.add_argument("INP_PATH", type=str, help="Path of the text file")
+PARSER.add_argument("INP_PATH",type=str,help="Path of the text file")
 
 ARGS = PARSER.parse_args()
 
 def redact_pii(given_path):    
-    PATH, FILE_NAME = os.path.split(given_path)
+	PATH, FILE_NAME = os.path.split(given_path)
 
-    # print("Path:", PATH)       
-    # print("Filename:", FILE_NAME)
+	# print("Path:", PATH)       
+	# print("Filename:", FILE_NAME)
+	
+	EMAIL_PATTERN = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+	
+	with open(given_path,'r') as file:
+	    CONTENT = file.read()
+	CONTENT = re.sub(EMAIL_PATTERN, '[REDACTED EMAIL]', CONTENT)
+	RANDOM_FILE_NAME = FILE_NAME.replace(".txt",'') +'_'+ str(uuid.uuid4())[0:7]+'.txt'
 
-    EMAIL_PATTERN = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+	with open(PATH +'\\' + RANDOM_FILE_NAME, 'w') as file :
+	    file.write(CONTENT)
+	# print(RANDOM_FILE_NAME)
 
-    with open(given_path, 'r') as file:
-        CONTENT = file.read()
-    CONTENT = re.sub(EMAIL_PATTERN, '[REDACTED EMAIL]', CONTENT)
-    RANDOM_FILE_NAME = FILE_NAME.replace(".txt", '') + '_' + str(uuid.uuid4())[0:7] + '.txt'
-
-    with open(os.path.join(PATH, RANDOM_FILE_NAME), 'w') as file:
-        file.write(CONTENT)
-    # print(RANDOM_FILE_NAME)
-    
     return CONTENT
 
 redact_pii(ARGS.INP_PATH)

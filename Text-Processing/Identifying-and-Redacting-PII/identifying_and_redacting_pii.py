e6a1fa7246219f539b6a7b4f93a92df4be6f3596
oneananda <anandathirumurugan@gmail.com>
2024-10-31 20:28:12 +0530

    unindent does not match any outer indentation level

--- Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
+++ Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
@@ -16,8 +16,7 @@ def redact_pii(given_path):
 	PATH, FILE_NAME = os.path.split(given_path)
 
 	# print("Path:", PATH)       
-	# print("Filename:", FILE_NAME)
-	
+	# print("Filename:", FILE_NAME)	
 	EMAIL_PATTERN = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 	
 	with open(given_path,'r') as file:

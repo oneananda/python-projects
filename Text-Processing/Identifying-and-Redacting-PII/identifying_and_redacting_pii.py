14bca4ad22c1531f56264a2e7a8276fd05ebcfce
oneananda <anandathirumurugan@gmail.com>
2024-11-03 08:41:50 +0530

    Moving masking_patterns to config

--- Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
+++ Text-Processing/Identifying-and-Redacting-PII/identifying_and_redacting_pii.py
@@ -80,54 +80,17 @@ def process_content(content, file_stamp):
     This function processes the content 
     and redacts Personally Identifiable Information (PII).
     """
+    # Define patterns for redacting
     redacting_patterns = config['patterns']['redacting_patterns']
     # Define patterns and corresponding masking functions
-    masking_patterns = {
-        'EMAIL': (
-            r'([a-zA-Z0-9._%+-])([a-zA-Z0-9._%+-]*)(@[\w.-]+\.[a-zA-Z]{2,})', 
-            lambda m: m.group(1) + "****" + m.group(3)
-        ),
-        'PHONE': (
-            r'\b(\+?\d{1,2})?[-.\s]?(\(?\d{3}\)?)?[-.\s]?(\d{3})[-.\s]?(\d{4})\b', 
-            lambda m: "***-***-" + m.group(4)
-        ),
-        'SSN': (
-            r'\b(\d{3})-(\d{2})-(\d{4})\b', 
-            lambda m: "***-**-" + m.group(3)
-        ),
-        'CREDIT_CARD': (
-            r'\b(\d{4})-?(\d{4})-?(\d{4})-?(\d{4})\b', 
-            lambda m: "****-****-****-" + m.group(4)
-        ),
-        'DATE_OF_BIRTH': (
-            r'\b(\d{2})[/-](\d{2})[/-](\d{4})\b', 
-            lambda m: "****/**/" + m.group(3)
-        ),
-        'PASSPORT': (
-            r'\b(\d{3})(\d{3})(\d{3})\b', 
-            lambda m: "***-***-" + m.group(3)
-        ),
-        'DRIVER_LICENSE': (
-            r'\b([A-Z]{1,2})-\d{3,6}-\d{3,6}\b', 
-            lambda m: m.group(1) + "-***-***"
-        ),
-        'BANK_ACCOUNT': (
-            r'\b\d{9,18}\b', 
-            lambda m: "****" + m.group(0)[-4:]
-        ),
-        'IP_ADDRESS': (
-            r'\b(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\b', 
-            lambda m: "***.***.***." + m.group(4)
-        ),
-        'MAC_ADDRESS': (
-            r'\b([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})\b', 
-            lambda m: "**:**:**:**:**:" + m.group(2)
-        ),
-        'PHYSICAL_ADDRESS': (
-            r'\b(\d+)\s([A-Za-z]+(?:\s[A-Za-z]+)*)\b', 
-            lambda m: "*** " + m.group(2)
-        )
-    }
+    # masking_patterns = config['patterns']['masking_patterns']
+
+    masking_patterns = {}
+    for pii_type, details in config['patterns']['masking_patterns'].items():
+        # Compile the pattern and convert the mask function from string to lambda
+        pattern = re.compile(details['pattern'])
+        mask_func = eval(details['mask_function'])  # Caution: Only use `eval` with trusted sources
+        masking_patterns[pii_type] = (pattern, mask_func)
 
     # Dictionary to store the count of redactions for each PII type
     redaction_count = {pii_type: 0 for pii_type in redacting_patterns}
